4f24cbf3aef8530d880b9a7ca1f950d8e43a249f
Fedor <kfedor@gmail.com>
2022-07-18 15:19:47 +0300

    remove examples from complex types

--- notifications/inapp/contract.go
+++ notifications/inapp/contract.go
@@ -20,11 +20,11 @@ type (
 
 	Parcel struct {
 		UserID      UserID                 `json:"userId" example:"users:john"`
-		Data        map[string]interface{} `json:"data" example:"any additional data"`
+		Data        map[string]interface{} `json:"data"`
 		ReferenceID string                 `json:"referenceId" example:"users:michael"`
 		Action      string                 `json:"action" example:"slaps you with a large trout"`
-		Actor       ID                     `json:"actor" example:""`
-		Subject     ID                     `json:"subject" example:""`
+		Actor       ID                     `json:"actor"`
+		Subject     ID                     `json:"subject"`
 	}
 
 	ID struct {
