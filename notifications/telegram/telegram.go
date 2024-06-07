33ec07450e4c055919248f7e0cb7617614cd6704
Myles <96409608+ice-myles@users.noreply.github.com>
2024-06-07 13:05:46 +0300

    Telegram notifications API. (#44)

---
+++ notifications/telegram/telegram.go
@@ -0,0 +1,171 @@
+// SPDX-License-Identifier: ice License 1.0
+
+package telegram
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	stdlibtime "time"
+
+	"github.com/goccy/go-json"
+	"github.com/imroc/req/v3"
+	"github.com/pkg/errors"
+
+	appcfg "github.com/ice-blockchain/wintr/config"
+	"github.com/ice-blockchain/wintr/log"
+)
+
+func init() { //nolint:gochecknoinits // It's the only way to tweak the client.
+	req.DefaultClient().SetJsonMarshal(json.Marshal)
+	req.DefaultClient().SetJsonUnmarshal(json.Unmarshal)
+	req.DefaultClient().GetClient().Timeout = requestDeadline
+}
+
+//nolint:funlen,gocognit,revive // .
+func New(applicationYAMLKey string) Client {
+	var cfg config
+	appcfg.MustLoadFromKey(applicationYAMLKey, &cfg)
+	if strings.TrimSpace(cfg.WintrTelegramNotifications.Credentials.BotToken) == "" {
+		module := strings.ToUpper(strings.ReplaceAll(strings.ReplaceAll(applicationYAMLKey, "-", "_"), "/", "_"))
+		cfg.WintrTelegramNotifications.Credentials.BotToken = os.Getenv(module + "_TELEGRAM_NOTIFICATIONS_CREDENTIALS_BOT_TOKEN")
+		if strings.TrimSpace(cfg.WintrTelegramNotifications.Credentials.BotToken) == "" {
+			cfg.WintrTelegramNotifications.Credentials.BotToken = os.Getenv("TELEGRAM_NOTIFICATIONS_CREDENTIALS_BOT_TOKEN")
+		}
+	}
+	if strings.TrimSpace(cfg.WintrTelegramNotifications.BaseURL) == "" {
+		module := strings.ToUpper(strings.ReplaceAll(strings.ReplaceAll(applicationYAMLKey, "-", "_"), "/", "_"))
+		cfg.WintrTelegramNotifications.BaseURL = os.Getenv(module + "_TELEGRAM_NOTIFICATIONS_BASE_URL")
+		if strings.TrimSpace(cfg.WintrTelegramNotifications.BaseURL) == "" {
+			cfg.WintrTelegramNotifications.BaseURL = os.Getenv("TELEGRAM_NOTIFICATIONS_BASE_URL")
+			if strings.TrimSpace(cfg.WintrTelegramNotifications.BaseURL) == "" {
+				cfg.WintrTelegramNotifications.BaseURL = "https://api.telegram.org"
+			}
+		}
+		if strings.HasSuffix(strings.TrimSpace(cfg.WintrTelegramNotifications.BaseURL), "/") {
+			cfg.WintrTelegramNotifications.BaseURL = cfg.WintrTelegramNotifications.BaseURL[:len(cfg.WintrTelegramNotifications.BaseURL)-1]
+		}
+	}
+	if strings.TrimSpace(cfg.WintrTelegramNotifications.ParseMode) == "" {
+		module := strings.ToUpper(strings.ReplaceAll(strings.ReplaceAll(applicationYAMLKey, "-", "_"), "/", "_"))
+		cfg.WintrTelegramNotifications.ParseMode = os.Getenv(module + "_TELEGRAM_NOTIFICATIONS_PARSE_MODE")
+		if strings.TrimSpace(cfg.WintrTelegramNotifications.ParseMode) == "" {
+			cfg.WintrTelegramNotifications.ParseMode = os.Getenv("TELEGRAM_NOTIFICATIONS_PARSE_MODE")
+			if strings.TrimSpace(cfg.WintrTelegramNotifications.ParseMode) == "" {
+				cfg.WintrTelegramNotifications.ParseMode = "HTML"
+			}
+		}
+	}
+	cl := &telegramNotification{
+		cfg: &cfg,
+	}
+	if err := cl.Send(
+		context.Background(), &Notification{ChatID: "test", Text: "test", BotToken: cfg.WintrTelegramNotifications.Credentials.BotToken},
+	); err != nil && !errors.Is(err, ErrTelegramNotificationBadRequest) {
+		log.Panic(err)
+	}
+
+	return cl
+}
+
+func (t *telegramNotification) Send(ctx context.Context, notif *Notification) error {
+	if ctx.Err() != nil {
+		return errors.Wrap(ctx.Err(), "context error")
+	}
+	msg, err := t.buildTelegramMessage(notif)
+	if err != nil {
+		return errors.Wrapf(err, "can't build telegram message for:%#v", notif)
+	}
+	url := fmt.Sprintf("%v/bot%v/sendMessage", t.cfg.WintrTelegramNotifications.BaseURL, notif.BotToken)
+	if sErr := t.post(ctx, url, msg); sErr != nil {
+		return errors.Wrapf(sErr, "can't call send() function for:%#v", notif)
+	}
+
+	return nil
+}
+
+func (t *telegramNotification) buildTelegramMessage(notif *Notification) (jsonVal string, err error) {
+	ts := telegramMessage{
+		ChatID:    notif.ChatID,
+		Text:      notif.Text,
+		ParseMode: t.cfg.WintrTelegramNotifications.ParseMode,
+		LinkPreviewOptions: struct {
+			URL           string `json:"url" example:"https://ice-staging.b-cdn.net/profile/default-profile-picture-1.png"`
+			IsDisabled    bool   `json:"is_disabled" example:"false"`    //nolint:tagliatelle // It's telegram API.
+			ShowAboveText bool   `json:"show_above_text" example:"true"` //nolint:tagliatelle // It's telegram API.
+		}{
+			IsDisabled:    t.cfg.WintrTelegramNotifications.LinkPreviewOptions.IsDisabled,
+			URL:           notif.PreviewImageURL,
+			ShowAboveText: true,
+		},
+		DisableNotification: notif.DisableNotification,
+	}
+	val, err := json.Marshal(ts)
+	if err != nil {
+		return "", errors.Wrapf(err, "can't send telegram push notification:%#v", notif)
+	}
+
+	return string(val), err
+}
+
+//nolint:funlen // .
+func (t *telegramNotification) post(ctx context.Context, url, body string) error {
+	newReq := t.buildHTTPRequest(ctx)
+	newReq = newReq.SetBodyJsonString(body)
+	resp, err := newReq.Post(url)
+	if err != nil || resp.IsErrorState() {
+		if err == nil {
+			respBody, pErr := resp.ToString()
+			if pErr != nil {
+				return errors.Wrapf(pErr, "notifications/telegram post `%v` failed, body:%#v, [1]unable to read response body", url, body)
+			}
+			var rErr error
+			switch resp.GetStatusCode() {
+			case 400: //nolint:gomnd,mnd // .
+				rErr = ErrTelegramNotificationBadRequest
+			case 403: //nolint:gomnd,mnd // .
+				rErr = ErrTelegramNotificationForbidden
+			case 404: //nolint:gomnd,mnd // .
+				rErr = ErrTelegramNotificationChatNotFound
+			default:
+				return errors.Wrapf(rErr, "notifications/telegram post `%v` failed, unexpected error, body:%#v, response: %v", url, body, respBody)
+			}
+
+			return errors.Wrapf(rErr, "notifications/telegram post `%v` failed, body:%#v, response: %v", url, body, respBody)
+		}
+
+		return errors.Wrapf(err, "notifications/telegram post `%v` failed, body:%#v", url, body)
+	}
+	if _, err = resp.ToString(); err != nil {
+		return errors.Wrapf(err, "notifications/telegram post `%v` failed, body:%#v, [2]unable to read response body", url, body)
+	}
+
+	return nil
+}
+
+//nolint:mnd,gomnd // Static config.
+func (*telegramNotification) buildHTTPRequest(ctx context.Context) *req.Request {
+	return req.
+		SetContext(ctx).
+		SetRetryBackoffInterval(10*stdlibtime.Millisecond, 1*stdlibtime.Second). //nolint:mnd,gomnd // .
+		SetRetryHook(func(resp *req.Response, err error) {
+			switch {
+			case err != nil:
+				log.Error(errors.Wrapf(err, "failed to send telegram notification, retrying... "))
+			case resp.GetStatusCode() == http.StatusTooManyRequests:
+				log.Error(errors.New("rate limit for telegram notification reached, retrying... "))
+			case resp.GetStatusCode() >= http.StatusInternalServerError:
+				log.Error(errors.New("failed to send telegram notification[internal server error], retrying... "))
+			}
+		}).
+		SetRetryCount(25).
+		SetRetryCondition(func(resp *req.Response, err error) bool {
+			return (err != nil ||
+				(resp.IsErrorState() && resp.GetStatusCode() != http.StatusBadRequest &&
+					resp.GetStatusCode() != http.StatusForbidden && resp.GetStatusCode() != http.StatusNotFound))
+		}).
+		SetContentType("application/json").
+		SetHeader("Accept", "application/json")
+}
---
+++ notifications/telegram/telegram_test.go
@@ -0,0 +1,239 @@
+// SPDX-License-Identifier: ice License 1.0
+
+package telegram
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+	"testing"
+	stdlibtime "time"
+
+	"github.com/stretchr/testify/require"
+
+	appcfg "github.com/ice-blockchain/wintr/config"
+)
+
+const (
+	testApplicationYAML = "self"
+	testPreviewImageURL = "https://ice-staging.b-cdn.net/profile/default-profile-picture-1.png"
+)
+
+// .
+var (
+	//nolint:gochecknoglobals // Because its loaded once, at runtime.
+	testChatID string
+)
+
+func TestMain(m *testing.M) {
+	module := strings.ToUpper(strings.ReplaceAll(strings.ReplaceAll(testApplicationYAML, "-", "_"), "/", "_"))
+	testChatID = os.Getenv(module + "_TELEGRAM_NOTIFICATIONS_TEST_CHAT_ID")
+	if strings.TrimSpace(testChatID) == "" {
+		testChatID = os.Getenv("TELEGRAM_NOTIFICATIONS_TEST_CHAT_ID")
+	}
+
+	os.Exit(m.Run())
+}
+
+//nolint:paralleltest // Not to have unpredictable too many attempts error.
+func TestClientSend_Success_WithPreview(t *testing.T) {
+	var cfg config
+	appcfg.MustLoadFromKey(testApplicationYAML, &cfg)
+	if cfg.WintrTelegramNotifications.Credentials.BotToken == "" || testChatID == "" {
+		t.Skip()
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*stdlibtime.Second)
+	defer cancel()
+	client := New(testApplicationYAML)
+	notif := &Notification{
+		ChatID:          testChatID,
+		Text:            "<b>test message</b>",
+		PreviewImageURL: testPreviewImageURL,
+		BotToken:        cfg.WintrTelegramNotifications.Credentials.BotToken,
+	}
+	require.NoError(t, client.Send(ctx, notif))
+}
+
+//nolint:paralleltest // Not to have unpredictable too many attempts error.
+func TestClientSend_Success_WithoutPreview(t *testing.T) {
+	var cfg config
+	appcfg.MustLoadFromKey(testApplicationYAML, &cfg)
+	if cfg.WintrTelegramNotifications.Credentials.BotToken == "" || testChatID == "" {
+		t.Skip()
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 30*stdlibtime.Second)
+	defer cancel()
+	client := New(testApplicationYAML)
+	notif := &Notification{
+		ChatID:   testChatID,
+		Text:     "<b>test message</b>",
+		BotToken: cfg.WintrTelegramNotifications.Credentials.BotToken,
+	}
+	require.NoError(t, client.Send(ctx, notif))
+}
+
+//nolint:paralleltest // Not to have unpredictable too many attempts error.
+func TestClientSend_Failure_NoBotInfo(t *testing.T) {
+	var cfg config
+	appcfg.MustLoadFromKey(testApplicationYAML, &cfg)
+	if cfg.WintrTelegramNotifications.Credentials.BotToken == "" || testChatID == "" {
+		t.Skip()
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*stdlibtime.Second)
+	defer cancel()
+	client := New(testApplicationYAML)
+	notif := &Notification{
+		ChatID:          testChatID,
+		Text:            "<b>test message</b>",
+		PreviewImageURL: "https://ice-staging.b-cdn.net/profile/default-profile-picture-1.png",
+		BotToken:        "",
+	}
+	require.ErrorIs(t, client.Send(ctx, notif), ErrTelegramNotificationChatNotFound)
+}
+
+//nolint:paralleltest // Not to have unpredictable too many attempts error.
+func TestClientSend_Failure_TooLongMessage(t *testing.T) {
+	var cfg config
+	appcfg.MustLoadFromKey(testApplicationYAML, &cfg)
+	if cfg.WintrTelegramNotifications.Credentials.BotToken == "" || testChatID == "" {
+		t.Skip()
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*stdlibtime.Second)
+	defer cancel()
+	client := New(testApplicationYAML)
+	var longText string
+	for range 4097 {
+		longText += "a"
+	}
+	notif := &Notification{
+		ChatID:   testChatID,
+		Text:     longText,
+		BotToken: cfg.WintrTelegramNotifications.Credentials.BotToken,
+	}
+	require.ErrorIs(t, client.Send(ctx, notif), ErrTelegramNotificationBadRequest)
+}
+
+//nolint:paralleltest // Not to have unpredictable too many attempts error.
+func TestClientSend_Failure_EmptyMessage(t *testing.T) {
+	var cfg config
+	appcfg.MustLoadFromKey(testApplicationYAML, &cfg)
+	if cfg.WintrTelegramNotifications.Credentials.BotToken == "" || testChatID == "" {
+		t.Skip()
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*stdlibtime.Second)
+	defer cancel()
+	client := New(testApplicationYAML)
+	notif := &Notification{
+		ChatID:   testChatID,
+		Text:     "",
+		BotToken: cfg.WintrTelegramNotifications.Credentials.BotToken,
+	}
+	require.ErrorIs(t, client.Send(ctx, notif), ErrTelegramNotificationBadRequest)
+}
+
+//nolint:paralleltest // Not to have unpredictable too many attempts error.
+func TestClientSend_Failure_NoChatId(t *testing.T) {
+	var cfg config
+	appcfg.MustLoadFromKey(testApplicationYAML, &cfg)
+	if cfg.WintrTelegramNotifications.Credentials.BotToken == "" {
+		t.Skip()
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*stdlibtime.Second)
+	defer cancel()
+	client := New(testApplicationYAML)
+	notif := &Notification{
+		ChatID:   "",
+		Text:     "<b>test message</b>",
+		BotToken: cfg.WintrTelegramNotifications.Credentials.BotToken,
+	}
+	require.ErrorIs(t, client.Send(ctx, notif), ErrTelegramNotificationBadRequest)
+}
+
+//nolint:paralleltest // Not to have unpredictable too many attempts error.
+func TestClientSendConcurrency_Success_TooManyAttempts(t *testing.T) {
+	var cfg config
+	appcfg.MustLoadFromKey(testApplicationYAML, &cfg)
+	if cfg.WintrTelegramNotifications.Credentials.BotToken == "" || testChatID == "" {
+		t.Skip()
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 60*stdlibtime.Second)
+	defer cancel()
+
+	client := New(testApplicationYAML)
+	wg := new(sync.WaitGroup)
+	const concurrency = 100
+	wg.Add(concurrency)
+	responder := make(chan error, concurrency)
+	for i := 0; i < concurrency; i++ { //nolint:intrange // .
+		notif := &Notification{
+			ChatID:   testChatID,
+			Text:     fmt.Sprintf("<b>test message %v</b>", i+1),
+			BotToken: cfg.WintrTelegramNotifications.Credentials.BotToken,
+		}
+		go func() {
+			defer wg.Done()
+			responder <- client.Send(ctx, notif)
+		}()
+	}
+	wg.Wait()
+	close(responder)
+	for err := range responder {
+		require.NoError(t, err)
+	}
+}
+
+func BenchmarkBufferedClientSendWithPreview(b *testing.B) {
+	var cfg config
+	appcfg.MustLoadFromKey(testApplicationYAML, &cfg)
+	if cfg.WintrTelegramNotifications.Credentials.BotToken == "" || testChatID == "" {
+		b.Skip()
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*stdlibtime.Second)
+	defer cancel()
+
+	client := New(testApplicationYAML)
+	b.SetParallelism(1000)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			notif := &Notification{
+				ChatID:          testChatID,
+				Text:            "<b>test message</b>",
+				PreviewImageURL: testPreviewImageURL,
+				BotToken:        cfg.WintrTelegramNotifications.Credentials.BotToken,
+			}
+			require.NoError(b, client.Send(ctx, notif))
+		}
+	})
+}
+
+func BenchmarkBufferedClientSendNoPreview(b *testing.B) {
+	var cfg config
+	appcfg.MustLoadFromKey(testApplicationYAML, &cfg)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*stdlibtime.Second)
+	defer cancel()
+	if cfg.WintrTelegramNotifications.Credentials.BotToken == "" || testChatID == "" {
+		b.Skip()
+	}
+
+	client := New(testApplicationYAML)
+	b.SetParallelism(1000)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			notif := &Notification{
+				ChatID:   testChatID,
+				Text:     "<b>test message</b>",
+				BotToken: cfg.WintrTelegramNotifications.Credentials.BotToken,
+			}
+			require.NoError(b, client.Send(ctx, notif))
+		}
+	})
+}
