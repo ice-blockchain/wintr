628d02d1d89a3a840e03ce3b156d4e533c0a7559
Dionysos <75300347+ice-dionysos@users.noreply.github.com>
2025-12-01 15:29:10 +0100

    ci: fix build

---
+++ connectors/fixture/fixture_build.go
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
+//go:build !test
+
+package fixture
--- server/fixture/http.go
+++ server/fixture/http.go
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: ice License 1.0
 
+//go:build test
+
 package fixture
 
 import (
